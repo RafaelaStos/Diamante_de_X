edecb68b7c806433f9a373ff6b398ad134e1a99c
Rafaela dos Santos Morais <morais.rafa508@gmail.com>
2022-02-21 16:36:04 -0300

    Adicionado a condiçãode numro inavalido

--- Diamante.Consoleapp/Program.cs
+++ Diamante.Consoleapp/Program.cs
@@ -8,66 +8,74 @@ namespace Diamante.Consoleapp
         {
             int num, espaco, asterisco = 1;
 
-            Console.WriteLine("Digite um numero impar: ");
-            num = int.Parse(Console.ReadLine());
+           
 
-            if (num % 2 != 0)
+            do
             {
-                espaco = (num - 1) / 2;
-                //parte de cima
-                for (int i = espaco; i > 0; i--)
-                {
-             
-                    for (int j = 1; j <= i; j++)
-                    {
+                Console.WriteLine("Digite um numero impar: ");
+                num = int.Parse(Console.ReadLine());
 
-                        Console.Write(" ");
-
-                    }
-                    for (int h = 1; h <= asterisco; h++)
-                    {
-                        
-                        Console.Write("X");
-                    }
-
-                    asterisco += 2;
-                    Console.WriteLine("");
-                }
-                //PARTE DO MEIO
-
-                for (int h = 0; h <asterisco; h++)
+                if (num % 2 != 0)
                 {
 
-                    Console.Write("X");
-                    
-                }
-
-
-                //parte de baixo
-                Console.WriteLine();
-                asterisco = num - 2;
-                for (int i = espaco; i > 0; i--)
-                {
-                    
-                    for (int j = i; j <= espaco; j++)
+                    espaco = (num - 1) / 2;
+                    //parte de cima
+                    for (int i = espaco; i > 0; i--)
                     {
 
-                        Console.Write(" ");
+                        for (int j = 1; j <= i; j++)
+                        {
 
+                            Console.Write(" ");
+
+                        }
+                        for (int h = 1; h <= asterisco; h++)
+                        {
+
+                            Console.Write("X");
+                        }
+
+                        asterisco += 2;
+                        Console.WriteLine("");
                     }
-                    for (int h = 1; h <= asterisco; h++)
+                    //PARTE DO MEIO
+
+                    for (int h = 0; h < asterisco; h++)
                     {
 
                         Console.Write("X");
+
                     }
-                    asterisco-=2;
-                    Console.WriteLine("");
+
+
+                    //parte de baixo
+                    Console.WriteLine();
+                    asterisco = num - 2;
+                    for (int i = espaco; i > 0; i--)
+                    {
+
+                        for (int j = i; j <= espaco; j++)
+                        {
+
+                            Console.Write(" ");
+
+                        }
+                        for (int h = 1; h <= asterisco; h++)
+                        {
+
+                            Console.Write("X");
+                        }
+                        asterisco -= 2;
+                        Console.WriteLine("");
+                    }
+                    break;
                 }
 
-            }
-            Console.ReadLine();
+                else Console.WriteLine("Numero invalido!");
 
-        
+
+                Console.ReadLine();
+            }while (num != 0);
         }
 
     }
